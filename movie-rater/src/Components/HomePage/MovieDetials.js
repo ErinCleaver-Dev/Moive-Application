fc66e5d3e7b3a670ffbaa42f4d4b39845f64fe25
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2021-05-09 19:06:48 -0400

    update

--- movie-rater/src/Components/HomePage/MovieDetials.js
+++ movie-rater/src/Components/HomePage/MovieDetials.js
@@ -3,14 +3,28 @@ import styled from 'styled-components'
 import StarRateIcon from '@material-ui/icons/StarRate';
 
 const MovieDetials = ({ movie } ) => {
- 
+    const displayStars = (movieRater) => {
+        let contant = [];
+        for(let i = 0; i <= 4; i++) {
+            contant.push(<Star className={movieRater.avg_rating > i ? 'orange': 'white'}/>)
+        }
+        return contant;
+    }
 
+    
     return (
-        <Container>
+        
+        <div>
+            { movie ? (
+            <Container>
             <h1>{movie.title}</h1>
             <p>{movie.description}</p>
+            <p>{displayStars(movie)}({movie.number_of_ratings})</p>
             
-        </Container>
+            </Container>
+            ) : null
+            } 
+        </div>
     )
 }
 
@@ -19,4 +33,11 @@ export default MovieDetials
 const Container = styled.div`
     flex: .75;
     padding: 10px;
+`
+
+const Star = styled(StarRateIcon)`
+
+    color: orange;
+
+
 `
